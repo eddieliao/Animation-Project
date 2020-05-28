c5e16bd8cd237e7390b97c9ffe6e958570afb955
s-liaoe <s-liaoe@51S500036477.it.bsd405.org>
2020-05-28 00:03:36 -0700

    started sprite code

---
+++ Animation/src/myMasterpiece/Sprite.java
@@ -0,0 +1,49 @@
+package myMasterpiece;
+
+import java.awt.event.KeyEvent;
+
+import processing.core.PGraphics;
+
+public class Sprite {
+	
+	private PGraphics g;
+	private KeyEvent key;
+	private int x;
+	private int y;
+	
+	public Sprite(PGraphics g, KeyEvent key, int x, int y)
+	{
+		this.g = g;
+		this.key = key;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void processMove()
+	{
+		if(key.getKeyCode() == KeyEvent.VK_W)
+		{
+			y++;
+		}
+		
+		if(key.getKeyCode() == KeyEvent.VK_A)
+		{
+			x--;
+		}
+		
+		if(key.getKeyCode() == KeyEvent.VK_S)
+		{
+			y--;
+		}
+		
+		if(key.getKeyCode() == KeyEvent.VK_D)
+		{
+			x++;
+		}
+	}
+	
+	public void drawSprite()
+	{
+		g.
+	}
+}
